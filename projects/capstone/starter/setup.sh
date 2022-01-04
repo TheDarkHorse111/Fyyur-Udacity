ac27cd211149c703f2d199491385b9afaa402562
Suliman <suliman.abuzaid98@gmail.com>
2022-01-04 23:58:22 +0200

    fixed wrong database name

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:postgres@localhost:5432/casting_agecny_db"
+export DATABASE_URL="postgresql://postgres:postgres@localhost:5432/casting_agency_db"
 export AUTH0_DOMAIN="dev-h2k020uf.us.auth0.com"
 export CLIENT_ID="F0AtspAGLjvAml2b4uy8yJjwND4nIi0E"
 export ALGORITHMS=['RS256']
